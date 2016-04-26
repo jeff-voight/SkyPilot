5c9557084254fca86cd9889f611d5478c048065a
jeff <jeff@MANGO>
2016-04-26 18:02:51 -0400

    Add project files.

---
+++ SkyPilot.ino
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+#include <LiquidCrystal_I2C.h>
+#include <dht11.h>
+
+
+
+void setup()
+{
+	Serial.begin(115200);
+  /* add setup code here */
+
+}
+
+void loop()
+{
+
+  /* add main program code here */
+
+}
